5b7d84345b15eb8976ddb67abfdb2e0a21216f22
flotob <florian@glatz-mahlberg.de>
2018-11-25 23:52:45 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -285,7 +285,7 @@
             offset: 200,
             duration: 600,
             easing: 'ease-in-sine',
-            delay: 300,
+            delay: 100,
             once: true,
             disable: 'mobile'
         });
